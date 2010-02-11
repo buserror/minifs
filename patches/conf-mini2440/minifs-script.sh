fc2a4393e1b200e6e1e0b1b087ba520c62c783cc
Michel Pollet <buserror@gmail.com>
2010-02-11 22:27:00 +0000

    MINI2440: Added a JFFS2 filesystem
    Untested
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- patches/conf-mini2440/minifs-script.sh
+++ patches/conf-mini2440/minifs-script.sh
@@ -7,7 +7,8 @@ TARGET_CFLAGS="-Os -march=armv4t -mtune=arm920t -mthumb-interwork -mthumb "
 
 function board_prepare() {
 	VERSION_linux=2.6.32.7
-	true
+
+#url[${#url[@]}]="http://www.opensource.apple.com/darwinsource/tarballs/other/mDNSResponder-107.6.tar.gz"
 }
 
 board_finish() {
@@ -16,4 +17,9 @@ board_finish() {
 
 board_compile() {
 	echo "board_compile"
+	# for a >64Mb nand with 2k blocks and 128k erase blocks
+	mkfs.jffs2 -q -l -p -e 0x20000 -s 0x800 \
+		-r "$ROOTFS" \
+		-o "$BUILD"/minifs-jffs2.img  \
+		-D "$BUILD"/special_file_table.txt
 }
