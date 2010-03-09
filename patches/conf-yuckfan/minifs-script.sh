6691b23f301bf95eefdef5e13d1ac3a85df4185e
Michel Pollet <buserror@gmail.com>
2010-03-09 20:49:01 +0000

    YUCKFAN: Added yuckfan board support
    Work in progress
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

---
+++ patches/conf-yuckfan/minifs-script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TARGET_ARCH=arm
+TARGET_FULL_ARCH=$TARGET_ARCH-v4-linux-uclibcgnueabi
+TARGET_KERNEL_NAME=uImage
+TARGET_CFLAGS="-Os -march=armv4t -mtune=arm920t -mthumb-interwork -mthumb "
+
+board_set_versions() {
+	VERSION_linux=2.6.32.7
+	# for a >64Mb nand with 2k blocks and 128k erase blocks
+	TARGET_FS_JFFS2="-q -l -p -e 0x20000 -s 0x800"
+	TARGET_FS_EXT_SIZE=32768
+	TARGET_SHARED=1
+}
+
+board_prepare() {
+	TARGET_PACKAGES+=" mtd_utils"
+	TARGET_PACKAGES+=" libftdi"
+
+	TARGET_PACKAGES+=" curl libexpat libreadline libiconv libnetsnmp libgettext"
+
+	# all of thqt for gtk
+	TARGET_PACKAGES+=" libjpeg libpng libfreetype libfontconfig libpixman"
+	TARGET_PACKAGES+=" libts libdirectfb"
+	TARGET_PACKAGES+=" libgtk"
+	
+	# all of gtk JUST to get rsvg :/
+	TARGET_PACKAGES+=" librsvg"
+}
+
