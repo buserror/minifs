864d161ac77ffd539f3e17e7804623cac0ff3ae5
Michel Pollet <buserror@gmail.com>
2010-04-01 15:18:36 +0100

    BIFF: Re-add initrd and add sensors
    Also add gdb/gdbserver
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- patches/conf-biff/minifs-script.sh
+++ patches/conf-biff/minifs-script.sh
@@ -4,8 +4,12 @@ TARGET_FULL_ARCH=$TARGET_ARCH-biff-linux-uclibc
 TARGET_KERNEL_NAME=bzImage
 TARGET_KERNEL_ARCH=x86
 TARGET_CFLAGS="-Os -march=i486"
+TARGET_INITRD=1
 
 board_prepare()
 {
 	TARGET_PACKAGES+=" libjpeg mjpg libusb libftdi"
+	if [ -d $HOME/Sources/Utils/sensors ]; then
+		TARGET_PACKAGES+=" sensors"
+	fi
 }
