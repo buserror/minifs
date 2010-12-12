f01a5e92e1d2eceb57a187f8ad8651b2f742af2e
Michel Pollet <buserror@gmail.com>
2010-12-12 11:11:23 +0000

    packages: Added bluez & btscanner
    And their binaries...
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/14bluez.sh
+++ conf/packages/14bluez.sh
@@ -15,3 +15,24 @@ PACKAGES+=" bluez"
 hset bluez url "http://www.kernel.org/pub/linux/bluetooth/bluez-4.81.tar.gz"
 hset bluez depends "dbus libiconv libgettext libglib"
 
+deploy-bluez() {
+	deploy cp $(get_installed_binaries) "$ROOTFS"/usr/bin/
+}
+
+PACKAGES+=" btscanner"
+hset btscanner url "http://www.pentest.co.uk/src/btscanner-2.1.tar.bz2"
+hset btscanner depends "bluez libxml2 libncurses"
+
+configure-btscanner() {
+	export LDFLAGS="$LDFLAGS_RLINK -lz"
+	export ac_cv_func_malloc_0_nonnull=yes
+	rm -f configure
+	sed -i -e 's|-Wimplicit-function-dec||g' configure.in
+	configure-generic 
+	export LDFLAGS="$LDFLAGS_BASE"
+	unset ac_cv_func_malloc_0_nonnull
+}
+
+deploy-btscanner() {
+	deploy cp $(get_installed_binaries) "$ROOTFS"/usr/bin/
+}
