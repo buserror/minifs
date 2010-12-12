7ddadd58459ffc316b1c87fa3186b56dbb56ca0d
Michel Pollet <buserror@gmail.com>
2010-12-12 11:13:07 +0000

    xorg: BIG changes, now use tagged 7.5
    Removed the git trees for xorg, and now use an
    official list of version for all modules extracted
    from the xorg website itself
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/12xorg.sh
+++ conf/packages/12xorg.sh
@@ -1,34 +1,53 @@
 
+
+XORG_VERSION=XORG-7_5
+
+# http://cgit.freedesktop.org/xorg/util/modular/plain/module-list.txt?h=XORG-7_5
+
+XORG_MODULES=""
+
+xorg_module_geturl() {
+	local path=$1
+	local module=$2
+
+	if [ "$XORG_MODULES" == "" ]; then
+		XORG_MODULES=$(cat "$PATCHES/packages/module-list-$XORG_VERSION.txt"|tr "\n" " ")
+	fi
+	mod=$(echo $XORG_MODULES|awk "{c=split(\$0,a); for(i=0;i<c;i++) if(match(a[i],/^$module-/)) print a[i] ;}")
+	url="http://xorg.freedesktop.org/archive/individual/$path/$mod.tar.bz2#xorg-$mod.tar.bz2"
+	echo $url
+}
+
 PACKAGES+=" xorgmacros"
-hset xorgmacros url "git!git://anongit.freedesktop.org/git/xorg/util/macros#xorgmacros-git.tar.bz2"
+hset xorgmacros url $(xorg_module_geturl "util" "util-macros")
 
 XORG_LIBS=""
 XORG_PROTOS=""
 XORG_FONTS=""
-
+# trapproto, xf86miscproto, evieext removed
 for p in \
-		x11proto dri2proto glproto xextproto bigreqsproto \
+		xproto dri2proto glproto xextproto bigreqsproto \
 		xcmiscproto fontsproto inputproto kbproto resourceproto \
-		scrnsaverproto videoproto recordproto trapproto \
-		xf86bigfontproto xf86dgaproto xf86miscproto \
+		scrnsaverproto videoproto recordproto  \
+		xf86bigfontproto xf86dgaproto  \
 		xf86vidmodeproto xf86driproto \
 		compositeproto damageproto fixesproto randrproto renderproto \
-		evieproto xineramaproto \
+		xineramaproto \
 ; do
 	XORG_PROTOS+=" xorg$p"
-	hset xorg$p url "git!git://anongit.freedesktop.org/git/xorg/proto/$p#xorg$p-git.tar.bz2"
+	hset xorg$p url $(xorg_module_geturl "proto" $p)
 	hset xorg$p depends "xorgmacros"
 done
 PACKAGES+=" $XORG_PROTOS"
 # make this one depends on all the others
-hset xorgx11proto depends "$XORG_PROTOS"
+hset xorgxproto depends "$XORG_PROTOS"
 
 # these are a bit special, they are needed by libxcb :/
 XCB_LIBS=""
-for p in libXau libxtrans ; do
+for p in libXau ; do
 	XCB_LIBS+=" xorg$p"
-	hset xorg$p url "git!git://anongit.freedesktop.org/git/xorg/lib/$p#xorg$p-git.tar.bz2"
-	hset xorg$p depends "xorgx11proto"
+	hset xorg$p url $(xorg_module_geturl "lib" $p)
+	hset xorg$p depends "xorgxproto"
 done
 PACKAGES+=" $XCB_LIBS"
 
@@ -40,31 +59,32 @@ hset libxcb depends "libpthreadstubs xcbproto $XCB_LIBS"
 hset xkeyboardconfig url "http://xlibs.freedesktop.org/xkbdesc/xkeyboard-config-1.7.tar.bz2"
 
 PACKAGES+=" xtrans" 
-hset xtrans url "http://www.x.org/releases/individual/lib/xtrans-1.2.5.tar.bz2"
+hset xtrans url $(xorg_module_geturl "lib" "xtrans")
 
 # libXfont needs "xmlto" tool on the host
 for p in  libXdmcp libX11 libfontenc libXfont libxkbfile \
 		libXrender libXft libXext libXfixes libXcursor libXdamage \
 		libXrandr libXcomposite libXinerama \
 		libXxf86vm libICE libSM libXt libXmu libXi libXv \
-		libpciaccess \
+		libpciaccess libXtst \
 		 ; do
 	XORG_LIBS+=" xorg$p"
-hset xorg$p url "git!git://anongit.freedesktop.org/git/xorg/lib/$p#xorg$p-git.tar.bz2"
+	hset xorg$p url $(xorg_module_geturl "lib" $p)
 done
 # stupid documentation breaks build
 configure-xorglibXfont() {
 	configure-generic --without-xmlto --without-fop
 }
-configure-xorglibX11() {
-	configure-generic
-	sed -i -e "s|^CFLAGS = .*|CFLAGS =|g" src/util/Makefile
-}
-hset xorglibX11 depends "libxcb xtrans xorgx11proto $XORG_LIBS"
+
+hset xorglibX11 depends "libxcb xtrans xorgxproto $XORG_LIBS"
 
 configure-xorglibXt() {
 	configure-generic
 	sed -i -e "s|^CFLAGS = .*|CFLAGS =|g" util/Makefile
+}
+
+configure-xorglibXfont() {
+	configure-generic --with-fop=elinks
 }
 
 configure-xorglibSM() {
@@ -75,8 +95,8 @@ configure-xorglibSM() {
 }
 
 XORG_FONTS+=" xorgfontutil xorgfontadobe"
-hset xorgfontutil url "http://www.x.org/releases/individual/font/font-util-1.1.1.tar.bz2"
-hset xorgfontadobe url "http://www.x.org/releases/individual/font/font-adobe-100dpi-1.0.1.tar.bz2"
+hset xorgfontutil url $(xorg_module_geturl "font" "font-util")
+hset xorgfontadobe url $(xorg_module_geturl "font" "font-adobe-100dpi")
 
 configure-xorgfontadobe() {
 	configure-generic --with-fontrootdir="/usr/share/fonts"
@@ -85,7 +105,7 @@ configure-xorgfontadobe() {
 PACKAGES+=" $XORG_LIBS $XORG_FONTS"
 
 PACKAGES+=" xkbcomp" 
-hset xkbcomp url "http://www.x.org/releases/individual/app/xkbcomp-1.1.1.tar.bz2"
+hset xkbcomp url $(xorg_module_geturl "app" "xkbcomp")
 hset xkbcomp depends "xkeyboardconfig xorglibxkbfile"
 
 configure-xkbcomp() {
@@ -97,7 +117,7 @@ configure-xkbcomp() {
 PACKAGES+=" libmesadrm libmesa"
 hset libmesadrm url "git!git://anongit.freedesktop.org/git/mesa/drm#libmesadrm-git.tar.bz2"
 #hset libmesa url "git!git://anongit.freedesktop.org/git/mesa/mesa#libmesa-git.tar.bz2"
-hset libmesa url "ftp://ftp.freedesktop.org/pub/mesa/7.8.2/MesaLib-7.8.2.tar.bz2"
+hset libmesa url "ftp://ftp.freedesktop.org/pub/mesa/7.7.1/MesaLib-7.7.1.tar.bz2"
 
 hset libmesa depends "libtalloc libmesadrm xorglibX11"
 
@@ -131,13 +151,14 @@ PACKAGES+=" libsha1"
 hset libsha1 url "git!git://github.com/dottedmag/libsha1.git#libsha1-git.tar.bz2"
 
 PACKAGES+=" xorgserver"
-hset xorgserver url "git!git://anongit.freedesktop.org/xorg/xserver#xorgserver-git.tar.bz2"
+#hset xorgserver url "git!git://anongit.freedesktop.org/xorg/xserver#xorgserver-git.tar.bz2"
+hset xorgserver url $(xorg_module_geturl "xserver" "xorg-server")
 hset xorgserver depends \
 	"busybox libsha1 xorglibX11 xorgfontutil \
 	xkbcomp xtrans \
 	xorgfontadobe \
 	xorginput-evdev xorginput-keyboard xorginput-mouse \
-	xorgvideo-fbdev libmesa"
+	xorgvideo-fbdev libmesa openssl"
 
 configure-xorgserver-local() {
 	export LDFLAGS="$LDFLAGS_RLINK"
@@ -181,7 +202,8 @@ for p in \
 		 ; do
 	PACKAGES+=" xorg$p"
 	fname=${p//-/}
-	hset xorg$p url "git!git://anongit.freedesktop.org/git/xorg/driver/xf86-$p#xorg$fname-git.tar.bz2"
+#	hset xorg$p url "git!git://anongit.freedesktop.org/git/xorg/driver/xf86-$p#xorg$fname-git.tar.bz2"
+	hset xorg$p url $(xorg_module_geturl "driver" "xf86-$p")
 	hset xorg$p depends "xorgserver"
 done
 
@@ -191,3 +213,32 @@ hset xorgvideo-nouveau depends "xorgserver libmesadrm"
 
 export X11_LIBS="$STAGING_USR/lib"
 
+
+
+configure-xorglibXv() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibXi() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibXt() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibX11() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibXrender() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibXrandr() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibXinerama() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibXext() {
+	configure-generic --enable-malloc0returnsnull
+}
+configure-xorglibXxf86vm() {
+	configure-generic --enable-malloc0returnsnull
+}
