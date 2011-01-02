3d4bcc6667dff33a31284fbb0de6623179be2ffb
Michel Pollet <buserror@gmail.com>
2011-01-02 14:41:52 +0000

    ppp: Simplified
    Removed unnecessary install bits
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/26ppp.sh
+++ conf/packages/26ppp.sh
@@ -2,9 +2,7 @@
 PACKAGES+=" ppp"
 hset ppp url "ftp://ftp.samba.org/pub/ppp/ppp-2.4.1.tar.gz"
 
-
 deploy-ppp() {
-        deploy cp $(get_installed_binaries) "$ROOTFS"/usr/bin/
-	deploy_staging_path "/etc/bluetooth"
+	deploy cp $(get_installed_binaries) "$ROOTFS"/bin/ 
 }
 
