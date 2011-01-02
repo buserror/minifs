a826092637570bc788f49f104a5175652a01c135
Michel Pollet <buserror@gmail.com>
2011-01-02 14:41:23 +0000

    install.c: Workaround stupid install usage
    packages (like ppp) use install wrongly. Updated
    install.c to remove chown/grp arguments, and to
    make sure the last argument is a directory, not
    a filename.
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/host-tools/install.c
+++ conf/host-tools/install.c
@@ -1,5 +1,7 @@
 // gcc -O -g 
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,9 +10,31 @@
 #include <poll.h>
 #include <unistd.h>
 
+char logname[256];
+FILE * o = NULL;
+
+
+#define TRACE(w) if (o) {\
+		fprintf(o, "%s %s: ", getenv("PACKAGE"), w);\
+		for (int i = 0; argv[i]; i++) fprintf(o, "%s ", argv[i]);\
+		fprintf(o,"\n"); fflush(o); \
+	}
+	
+#define V(w) { \
+	if (!o) {\
+		o = fopen(logname, "a"); \
+		if (!o) perror(logname);\
+		TRACE("START");\
+	}\
+	if (o) { w ; fflush(o); } \
+}
+
 int main(int argc, char * argv[])
 {
 	const char *real_install = getenv("HOST_INSTALL");
+	char fix[8192];
+	
+	sprintf(logname, "%s/._install_warnings.log", getenv("BUILD"));
 
 	if (!real_install) {
 		fprintf(stderr, "fake install needs it's real one !! bailing.\n");
@@ -18,21 +42,47 @@ int main(int argc, char * argv[])
 	}
 	char * staging = getenv("STAGING");
 	char * base = getenv("MINIFS_BASE");
+	
+	for (int i = 1; i < argc; ) {
+		int del = 0;
+		if (!strcmp(argv[i], "-o") && i < argc-1) {
+			V(fprintf(o, "%s: Preventing chown %s on %s\n", getenv("PACKAGE"), argv[i+1], argv[argc-1]);)
+			del = 2;
+		} else if (!strcmp(argv[i], "-g") && i < argc-1) {
+			V(fprintf(o, "%s: Preventing chgrp %s on %s\n", getenv("PACKAGE"), argv[i+1], argv[argc-1]);)
+			del = 2;
+		}
+		if (del) {
+			int cnt = argc-i-del+1;
+			printf("install moving %d/%d elements from index %d to %d\n", cnt, argc, i+del, i);
+			memmove(&argv[i], &argv[i+del], cnt * sizeof(char*));
+			argc -= del;
+		} else
+			i++;
+	}
 	char * last = argv[argc-1];
-	char fix[4096];
 	if (last[0] == '/' && strncmp(last, base, strlen(base))) {
-		sprintf(fix, "%s/._install_warnings.log", getenv("BUILD"));
-		FILE * o = fopen(fix, "a");
 		if (strncmp(last, "/usr", 4) && getenv("INSTALL_USR"))
 			sprintf(fix, "%s/usr%s", staging, last);
 		else
 			sprintf(fix, "%s%s", staging, last);
 		argv[argc-1] = fix;
-		if (o) {
-			fprintf(o, "%s: Fixing %s to staging\n", getenv("PACKAGE"), last);
-			fclose(o);
+		
+		V(fprintf(o, "%s: Fixing %s to staging\n", getenv("PACKAGE"), last);)
+	}
+	struct stat st;
+	if (stat(last, &st) == 0) {
+		if (!S_ISDIR(st.st_mode)) {
+			V(fprintf(o, "%s: Fixing %s to a directory name\n", getenv("PACKAGE"), last);)
+			char *l = strrchr(last, '/');
+			if (l)
+				l[1] = 0; // strip filename
 		}
 	}
+	if (o) {
+		TRACE("END");
+		fclose(o);
+	}	
 	execvp(real_install, argv);
 	perror(argv[0]);
 	exit(127);
