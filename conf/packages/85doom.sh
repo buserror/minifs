47b2b3ad71e8cbc6816d686a9c612a9deda2bdba
Michel Pollet <buserror@gmail.com>
2011-01-04 23:59:48 +0000

    DOOOOOM: Add a SDL demo (doom)
    Not working in 8MB memory
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

---
+++ conf/packages/85doom.sh
@@ -0,0 +1,26 @@
+PACKAGES+=" sdldoom"
+hset sdldoom url "http://www.libsdl.org/projects/doom/src/sdldoom-1.10.tar.gz"
+hset sdldoom depends "libsdl"
+
+download-sdldoom() {
+	if [ ! -f doom1.wad.gz ]; then
+		wget "http://www.libsdl.org/projects/doom/data/doom1.wad.gz"
+	fi
+}
+
+configure-sdldoom() {
+	configure-generic
+}
+
+deploy-sdldoom-local() {
+	set -x
+	mkdir -p "$ROOTFS"/usr/share/
+	gzip -dc "$BASE"/download/doom1.wad.gz \
+		>"$ROOTFS"/usr/share/doom1.wad
+    cp $(get_installed_binaries) "$ROOTFS"/usr/bin/
+	set +x
+}
+
+deploy-sdldoom() {
+	deploy deploy-sdldoom-local
+}
