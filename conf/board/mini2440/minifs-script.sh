ed4189718a1319093b91be7fa154ec157afcaaad
Michel Pollet <buserror@gmail.com>
2011-01-06 23:18:39 +0000

    mini2440: Added u-boot to the build
    Copied to build-mini2440/
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/board/mini2440/minifs-script.sh
+++ conf/board/mini2440/minifs-script.sh
@@ -10,9 +10,15 @@ board_set_versions() {
 	# for a >64Mb nand with 2k blocks and 128k erase blocks
 	TARGET_FS_JFFS2="-q -l -p -e 0x20000 -s 0x800"
 	TARGET_INITRD=1
+	hset uboot url "git!git://repo.or.cz/u-boot-openmoko/mini2440.git#uboot-mini2440-git.tar.bz2"
 }
 
 board_prepare() {
-#	TARGET_PACKAGES+=" zlib lzo e2fsprogs"
+	TARGET_PACKAGES+=" uboot"
+}
+
+mini2440-deploy-uboot() {
+	# make sure the u-boot is aligned on 2k blocks, for mtd_debug
+	deploy dd if=u-boot.bin of="$BUILD"/u-boot.bin bs=2048 conv=sync
 }
 
