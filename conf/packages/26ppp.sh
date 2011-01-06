debfb52ca97d8885c75f320b3d27776e17d8a449
Michel Pollet <buserror@gmail.com>
2011-01-06 16:41:51 +0000

    ppp: Fix permissions again
    Stupid script installs 0555 which makes "strip" fail later
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/26ppp.sh
+++ conf/packages/26ppp.sh
@@ -2,7 +2,12 @@
 PACKAGES+=" ppp"
 hset ppp url "ftp://ftp.samba.org/pub/ppp/ppp-2.4.1.tar.gz"
 
+install-ppp() {
+	install-generic
+	chmod 0755 $(get_installed_binaries)
+}
+
 deploy-ppp() {
-	deploy cp $(get_installed_binaries) "$ROOTFS"/bin/ 
+	deploy cp $(get_installed_binaries) "$ROOTFS"/bin/
 }
 
