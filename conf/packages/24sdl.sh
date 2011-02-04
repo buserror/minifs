e77b393ebe08981efb456ba4d438521743186d2c
Michel Pollet <buserror@gmail.com>
2011-02-04 14:01:18 +0000

    sdl: Now look for xorg
    If xorg is there, compile a SDL driver for it...

--- conf/packages/24sdl.sh
+++ conf/packages/24sdl.sh
@@ -4,8 +4,13 @@ PACKAGES+=" libsdl"
 hset libsdl url "http://www.libsdl.org/release/SDL-1.2.14.tar.gz"
 
 configure-libsdl() {
+	local xorg=$(echo $TARGET_PACKAGES|awk '/xorg/{print "found"}')
+	local extras=""
+	if [ "$xorg" != "found" ]; then
+		extras+="--without-x"
+	fi
 	configure-generic \
-		--without-x \
+		$extras \
 		--enable-arts=no \
 		--enable-esd=no \
 		--enable-pulseaudio=no \
