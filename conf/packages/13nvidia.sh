7f5556ed037fc0bca964359750594936f257d68a
Michel Pollet <buserror@gmail.com>
2011-02-04 13:56:04 +0000

    nvidia: Added a gawk replacement installer
    lucky nvidia comes with a pretty complete manifest file
    New script parses it and generate an installer script that'll
    do all the gorry bits.
    
    Tested, works now.
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/13nvidia.sh
+++ conf/packages/13nvidia.sh
@@ -5,6 +5,9 @@ PACKAGES+=" nvidia"
 hset nvidia url "http://us.download.nvidia.com/XFree86/Linux-x86/$NVIDIA_VERSION/$NVIDIA_NAME.run"
 hset nvidia depends "xorgserver linux-modules"
 
+setup-nvidia() {
+	ROOTFS_KEEPERS+="libvdpau.so:"
+}
 uncompress-nvidia() {
 	echo nvidia: $*
 	sh $2 -x
@@ -33,9 +36,12 @@ compile-nvidia() {
 
 install-nvidia-local() {
 	set -x
-	pushd $NVIDIA_NAME/kernel
+	pushd $NVIDIA_NAME/
 	mkdir -p "$BUILD"/kernel/lib/modules/$(hget linux version)/kernel/drivers/video/
-	cp nvidia.ko "$BUILD"/kernel/lib/modules/$(hget linux version)/kernel/drivers/video/
+	cp kernel/nvidia.ko "$BUILD"/kernel/lib/modules/$(hget linux version)/kernel/drivers/video/
+	sh "$PATCHES/patches/nvidia/nvidia-minifs-installer.sh" >../._nvidia_install.sh
+	DESTDIR="$STAGING_USR" installwatch -o ../._dist_$PACKAGE.log bash ../._nvidia_install.sh
+	ln -sf libGL.so.1 "$STAGING_USR"/lib/libGL.so.1.2
 	popd
 	set +x
 }
@@ -44,3 +50,8 @@ install-nvidia() {
 	log_install install-nvidia-local
 }
 
+deploy-nvidia() {
+	mkdir -p "$ROOTFS"/etc/X11
+	deploy cp $(get_installed_binaries) \
+		"$ROOTFS"/usr/bin/
+}
---
+++ conf/patches/nvidia/nvidia-minifs-installer.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#
+# This script replaces the nvidia binary installer. It reads the
+# .manifest file describing what to install, and does it with a
+# rather large awk script.
+# This script generates /another/ script as output, that can
+# then be evaluated
+#
+DESTDIR="\$DESTDIR"
+export OPENGL_LIB=$DESTDIR/lib
+export OPENGL_SYMLINK=$OPENGL_LIB
+export LIBGL_LA=$OPENGL_LIB
+export OPENGL_HEADER=$DESTDIR/include
+export XMODULE_SHARED_LIB=$DESTDIR/lib/xorg/modules
+export XMODULE_SYMLINK=$XMODULE_SHARED_LIB
+export XMODULE_NEWSYM=$XMODULE_SHARED_LIB
+export TLS_LIB=$DESTDIR/lib
+
+export XLIB_STATIC_LIB=$DESTDIR/lib
+export XLIB_SHARED_LIB=$XLIB_STATIC_LIB
+export XLIB_SYMLINK=$XLIB_SHARED_LIB
+export UTILITY_BINARY=$DESTDIR/bin
+export UTILITY_BIN_SYMLINK=$UTILITY_BINARY
+export UTILITY_LIB=$DESTDIR/lib
+export UTILITY_LIB_SYMLINK=$UTILITY_LIB
+
+export CUDA_ICD=$DESTDIR/etc/OpenCL/vendors
+export CUDA_LIB=$DESTDIR/lib
+export CUDA_SYMLINK=$CUDA_LIB
+
+export VDPAU_LIB=$DESTDIR/lib
+export VDPAU_SYMLINK=$VDPAU_LIB
+export NVCUVID_LIB=$DESTDIR/lib
+export NVCUVID_LIB_SYMLINK=$NVCUVID_LIB
+
+echo "set -x"
+cat .manifest | gawk "
+function env(what, extra) {
+	if (!(what in ENVIRON)) {
+		print \"  #\", what, \" is not defined\"
+		return \"\"
+	}
+	dir=ENVIRON[what] \"/\" extra;
+	if (!(dir in dirmade)) {
+		printf(\"mkdir -p %s\n\", dir);
+		dirmade[dir] = 1
+	}
+	return dir
+}
+{
+	if (header++ < 8) next;
+	fil=\$1;
+	perm=\$2;
+	dest=\$3;
+
+	switch (dest) {
+		case /^DOC/:
+		case /^MAN/:
+		case /SRC$/:
+		case /DESKTOP$/:
+		case /INSTALLER_BINARY/:
+		case /NEWSYM/: # not needed
+		#	print \"skip\", dest;
+			next;
+	}
+	path=\"\"
+	for (i = 4; i < NF; i++) {
+		switch (\$(i)) {
+			case \"NATIVE\":
+			case \"NEW\":
+			case \"CLASSIC\":
+			case \"/\":
+				break;
+			default:
+				path=path \$(i)
+		}
+	}
+		
+	switch (dest) {
+		case /SYMLINK/:
+			printf(\"ln -f -s %s %s%s\\n\", \$(NF), env(dest,path), fil);
+			next;
+		
+		default:
+			printf(\"cp -fa %s %s%s\\n\", fil, env(dest, path), match(\$(NF), /^[A-Z]/) ? \"\" : \$(NF));
+			next;
+	}
+	print \"  # ERROR, WHATS THAT? \", dest, fil, NF
+}"
