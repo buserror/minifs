d9fa6c3317c70340fdf06fe9a75a5794d4e8ef78
Michel Pollet <buserror@gmail.com>
2011-04-17 10:13:32 +0100

    atom: Tweaked boot code
    run udhcpd in the background, but make it run /etc/network-up.sh
    when it has a lease, so we can start xorg etc in the meantime
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/board/atom/rootfs/etc/xserver-start.sh
+++
@@ -1,7 +0,0 @@
-
-Xfbdev :0 -mouse mouse -keybd keyboard -dpi 100 -rgba rgb
-export WEBKIT_DEBUG=1
-GtkLauncher --display :0 http://www.tvguide.co.uk/tv_channel_streams.asp\?c=16 &
-
-
-X :0 -dpi 100 -ac -config xorg.conf.new &
