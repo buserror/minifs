89fd5c3b1d6e9f82f9781ed223f788fbfbac3ed7
Michel Pollet <buserror@gmail.com>
2011-05-26 18:21:01 +0100

    libvorbis: Updated
    New version also builds better
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/15webkit.sh
+++ conf/packages/15webkit.sh
@@ -28,7 +28,7 @@ PACKAGES+=" libogg"
 hset libogg url "http://downloads.xiph.org/releases/ogg/libogg-1.1.4.tar.gz"
 
 PACKAGES+=" libvorbis"
-hset libvorbis url "http://downloads.xiph.org/releases/vorbis/libvorbis-1.2.3.tar.gz"
+hset libvorbis url "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.2.tar.gz"
 
 configure-libvorbis() {
 	export LDFLAGS="$LDFLAGS_RLINK"
