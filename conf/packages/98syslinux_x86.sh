10f618c53bf04acb400305466300866394962573
Michel Pollet <buserror@gmail.com>
2011-05-26 18:23:54 +0100

    syslinux: Aborted attempt at building in target
    Doesn't work yet
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/98syslinux_x86.sh
+++ conf/packages/98syslinux_x86.sh
@@ -1,9 +1,22 @@
 
 PACKAGES+=" syslinux"
-hset syslinux url "http://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-4.03.tar.bz2"
+hset syslinux url "http://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-4.04.tar.bz2"
 #hset syslinux depends "linux-modules"
 hset syslinux phases "deploy"
 
+compile-syslinux() {
+	compile-generic \
+		CC="$CC" LD="$LD" \
+		CFLAGS="$CFLAGS" \
+		LDFLAGS="$LDFLAGS_RLINK"
+}
+
+install-syslinux() {
+	install-generic \
+		CC="$CC" LD="$LD" \
+		CFLAGS="$CFLAGS" \
+		LDFLAGS="$LDFLAGS_RLINK"
+}
 
 deploy-syslinux() {
 	deploy echo Done
