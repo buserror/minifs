a0a48f27a8f2e4009a87330fadbcc93dd138af46
Michel Pollet <buserror@gmail.com>
2011-09-25 11:26:10 +0100

    uboot: Install mkimage
    Uses our host mkimage, so we don't need the hosts distro's one
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/06uboot.sh
+++ conf/packages/06uboot.sh
@@ -10,3 +10,13 @@ compile-uboot() {
 	compile make CROSS_COMPILE="$CROSS-"
 }
 
+install-uboot-local() {
+	if [ -x tools/mkimage ]; then
+		cp tools/mkimage "$STAGING_TOOLS"/bin/
+	fi
+}
+
+install-uboot() {
+	log_install install-uboot-local
+}
+
