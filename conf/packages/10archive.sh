507b3332b0196090c1452b4686fd276a3de7af52
Michel Pollet <buserror@gmail.com>
2011-09-25 11:36:09 +0100

    libarchive: New package
    Compiles without conflicting with busybox now
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

---
+++ conf/packages/10archive.sh
@@ -0,0 +1,9 @@
+
+#######################################################################
+## libarchive
+#######################################################################
+
+PACKAGES+=" libarchive"
+hset libarchive url "http://libarchive.googlecode.com/files/libarchive-2.8.5.tar.gz"
+hset libarchive depends "zlib"
+
