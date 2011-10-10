f80ca57d7627511b1f04549b7254f965be4cdd01
Michel Pollet <buserror@gmail.com>
2011-10-10 08:28:26 +0100

    mdnsresponder: Upgraded
    Added the shared lib and the daemon to builds
    Fixes compilation..
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/80mdnsresponder.sh
+++ conf/packages/80mdnsresponder.sh
@@ -1,27 +1,50 @@
-#######################################################################
-## mDSNResponder
-#######################################################################
-PACKAGES+=" mDSNResponder"
-V="107.6"
-hset mDSNResponder version $V
-hset mDSNResponder url "http://www.opensource.apple.com/darwinsource/tarballs/other/mDNSResponder-$V.tar.gz"
-hset mDSNResponder depends "busybox"
 
-configure-mDSNResponder() {
+#######################################################################
+## mDNSResponder
+#######################################################################
+PACKAGES+=" mDNSResponder"
+#V="107.6"
+V="320.5.1"
+hset mDNSResponder version $V
+hset mDNSResponder url "http://www.opensource.apple.com/darwinsource/tarballs/other/mDNSResponder-$V.tar.gz"
+hset mDNSResponder depends "busybox"
+
+configure-mDNSResponder() {
 	configure echo Done
 }
 
-compile-mDSNResponder() {
-	compile $MAKE -C mDNSPosix \
-		os=linux CC="$GCC" SAResponder \
+compile-mDNSResponder-local() {
+	$MAKE -C mDNSPosix \
+		libdns_sd \
+		os=linux CC="ccfix $TARGET_FULL_ARCH-gcc" STRIP="$TARGET_FULL_ARCH-strip" \
+		LD="$TARGET_FULL_ARCH-gcc" \
+		CFLAGS_CROSS="-Os $TARGET_CFLAGS -I$STAGING/include" \
+		LINKOPTS="$LDFLAGS -shared -Wl,-soname,libdns_sd.so" &&
+	$MAKE -C mDNSPosix \
+		SAResponder  Daemon \
+		os=linux CC="ccfix $TARGET_FULL_ARCH-gcc" STRIP="$TARGET_FULL_ARCH-strip" \
+		LD="$TARGET_FULL_ARCH-ld" \
 		CFLAGS_CROSS="-Os $TARGET_CFLAGS -I$STAGING/include" \
 		LINKOPTS="$LDFLAGS"
 }
-
-install-mDSNResponder() {
-	log_install cp mDNSPosix/build/prod/mDNSResponderPosix "$STAGING_USR"/bin/
+compile-mDNSResponder() {
+	compile compile-mDNSResponder-local
 }
 
-deploy-mDSNResponder() {
-	deploy cp "$STAGING_USR"/bin/mDNSResponderPosix "$ROOTFS"/usr/bin/
+install-mDNSResponder-local() {
+	cp mDNSPosix/build/prod/mDNSResponderPosix "$STAGING_USR"/bin/
+	cp mDNSPosix/build/prod/mdnsd "$STAGING_USR"/bin/
+	cp mDNSPosix/build/prod/libdns_sd.so "$STAGING_USR"/lib/
+	cp mDNSShared/dns_sd.h "$STAGING_USR"/include/
+}
+
+install-mDNSResponder() {
+	log_install install-mDNSResponder-local
+}
+
+deploy-mDNSResponder() {
+	deploy cp \
+		"$STAGING_USR"/bin/mDNSResponderPosix \
+		"$STAGING_USR"/bin/mdnsd \
+		"$ROOTFS"/usr/bin/
 }
