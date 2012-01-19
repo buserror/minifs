5f6a422c08016c1637c9c0ad15d4fcb810656dc9
Michel Pollet <buserror@gmail.com>
2012-01-19 11:20:21 +0000

    vnc: Updated, fixed again
    Uses 0.9.8 now.. Also cleaned up the config choices
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/25vnc.sh
+++ conf/packages/25vnc.sh
@@ -2,36 +2,46 @@
 
 
 PACKAGES+=" libvncserver"
-hset libvncserver url "http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.8.2/LibVNCServer-0.9.8.2.tar.gz"
+hset libvncserver url "http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.7/LibVNCServer-0.9.7.tar.gz"
+#hset libvncserver url "http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.8.2/LibVNCServer-0.9.8.2.tar.gz"
 hset libvncserver depends "zlib libjpeg"
 hset libvncserver configscript "libvncserver-config"
-hset libvncserver optional "libsdl xorglibX11"
+hset libvncserver optional "libsdl xorglibX11 libgcrypt"
 
 configure-libvncserver-local() {
 	set -x
 	local extras=""
-	local sdl=$(echo $TARGET_PACKAGES|awk '/libsdl/{print "found"}')
-	local xorg="not there"
+
+	export LDFLAGS="$LDFLAGS_RLINK"
+
 	# that bit needs SDL somehow. we don't want it if SDL not built
-	if [ "$sdl" != "found" ]; then
+	if ! env_contains TARGET_PACKAGES libsdl ; then 
 		echo libvncserver Disabling SDL 
 		sed -i -e 's|client_examples||g' \
 			Makefile.am
 		sed -i -e "/client_examples\/Makefile/d" \
 			configure.ac
 	fi
-	if [ "$xorg" != "found" ]; then
-		echo libvncserver Disabling x11vnc 
-		extras="--without-x11vnc"
-		sed -i -e 's|x11vnc||g' \
-				Makefile.am
-		sed -i -e "s|[ \t]\(AC_CONFIG_FILES(\[x11vnc/Makefile\)|true #\1|g" \
-				configure.ac
+	if env_contains TARGET_PACKAGES xorglibX11 ; then 
+		if env_contains TARGET_PACKAGES libxcb; then
+			LDFLAGS+=" -lxcb"
+		fi
 	fi
+	if ! env_contains TARGET_PACKAGES libgcrypt; then
+		extras+=" --without-gcrypt"
+	fi
+	echo libvncserver Disabling x11vnc 
+	extras+=" --without-x11vnc"
+	sed -i -e 's|x11vnc||g' \
+			Makefile.am
+	sed -i -e "s|[ \t]\(AC_CONFIG_FILES(\[x11vnc/Makefile\)|true #\1|g" \
+			configure.ac
+	
 	libtoolize && $ACLOCAL && autoheader && automake --force-missing --foreign -a -c && autoconf
-	configure-generic-local --without-gcrypt $extras
+	configure-generic-local $extras
 	# 0.9.8.2 uses some crap windows file because of /* #undef LIBVNCSERVER_HAVE_GETTIMEOFDAY */
 	sed -i -e 's|/\* #undef LIBVNCSERVER_HAVE_GETTIMEOFDAY \*/|#define LIBVNCSERVER_HAVE_GETTIMEOFDAY 1|' rfb/rfbconfig.h
+	export LDFLAGS="$LDFLAGS_BASE"
 	set +x
 }
 
