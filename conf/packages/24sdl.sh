e84d17ca95df4c2cc2cc8502a6f6e0979bf8aa98
Michel Pollet <buserror@gmail.com>
2012-09-04 12:53:38 +0100

    SDL: Added a few tools, inv gmenu2x
    Thanks to ReggieUK for the patches
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/24sdl.sh
+++ conf/packages/24sdl.sh
@@ -35,7 +35,7 @@ hset libsdlimage optional "libjpeg"
 
 setup-libsdlimage() {
 	# SDL_image manualy load shared libs, they are not (GGRRRRR!) in ELF 
-	ROOTFS_KEEPERS+="libpng14.so:libjpeg.so:"
+	ROOTFS_KEEPERS+="libpng14.so:libjpeg.so:libSDL_image.so:libSDL_image-1.2.so:"
 }
 
 PACKAGES+=" libsdlttf"
@@ -105,5 +105,82 @@ deploy-kobodeluxe() {
 	deploy deploy_binaries
 	cp -rf "$STAGING_USR"/com "$ROOTFS"/usr/
 	cp -rf "$STAGING_USR"/share/kobo-deluxe "$ROOTFS"/usr/share/
+    # we need to run kobodeluxe with some switches, so we really need to run it with a script
+    # move the binary to there
+    mv "$ROOTFS"/usr/bin/kobodl "$ROOTFS"/usr/share/kobo-deluxe/
+    # now create the script
+    echo "#!/bin/hush" > "$ROOTFS"/usr/sbin/kobodl
+    echo "cd /usr/share/kobo-deluxe/" > "$ROOTFS"/usr/sbin/kobodl
+    echo "./kobodl -width 320 -height 240 -fullscreen -nosound &" > "$ROOTFS"/usr/sbin/kobodl
+    #make sure the script is executable, you can now run it 
+    chmod a+wrx "$ROOTFS"/usr/sbin/kobodl
 	
+}
+
+PACKAGES+=" libsdlgfx"
+hset libsdlgfx url "http://www.ferzkopp.net/Software/SDL_gfx-2.0/SDL_gfx-2.0.22.tar.gz"
+hset libsdlgfx depends "libsdl"
+
+configure-libsdlgfx() {
+	export SDL_CONFIG="$STAGING_TOOLS"/bin/sdl-config
+        # autocrap wants a m4 directory , so we create it if not already done
+	if [ ! -e m4 ]; then mkdir m4; fi
+	configure-generic \
+		--disable-mmx --without-x --disable-sdltest
+}
+
+deploy-libsdlgfx(){
+	# we use ROOTFS_KEEPERS to tell minifs to keep stuff, it will look in standard install directories for the keepers, so they
+	# stay in the ROOTFS for final deployment into our .img file :)
+	ROOTFS_KEEPERS+="libSDL_gfx.so:"
+}
+
+PACKAGES+=" gmenu2x"
+hset gmenu2x url "git!git://projects.qi-hardware.com/gmenu2x.git#gmenu2x-git.tar.bz2"
+hset gmenu2x depends "libsdl libsdlgfx libsdlimage libpng"
+ 
+configure-gmenu2x() {
+	export SDL_CONFIG="$STAGING_TOOLS"/bin/sdl-config
+    export LDFLAGS="$LDFLAGS_RLINK"
+	configure-generic --without-x --disable-sdltest --enable-platform=gp2x
+	# We're using the gp2x platform flag, not entirely sure how significant this is
+	# but it's a closer target to the frame than anything else i think
+	# eventually we'll have our own specific platform, we need to create a platform folder
+	# for the gp2x otherwise the installer will fail, so we snarf the pandora one and copy it :D
+	export LDFLAGS="$LDFLAGS_BASE"
+	cp -rf data/platform/pandora data/platform/gp2x
+}
+
+deploy-gmenu2x-local() {
+	cp -rf "$STAGING_USR"/share/gmenu2x/ "$ROOTFS"/usr/share/
+    cp $(get_installed_binaries) "$ROOTFS"/usr/share/gmenu2x
+    # hacky fix to create a script file, should probably create a patch that makes the file in conf/patches/
+	echo "#!/bin/hush" > "$ROOTFS"/usr/sbin/gmenu2x
+	echo "cd /usr/share/gmenu2x/" >> "$ROOTFS"/usr/sbin/gmenu2x
+	echo "./gmenu2x &" >> "$ROOTFS"/usr/sbin/gmenu2x
+	chmod a+wrx "$ROOTFS"/usr/sbin/gmenu2x
+}
+
+deploy-gmenu2x() {
+    ROOTFS_KEEPERS+="libstdc++.so:"
+    # the deploy command is part of minifs's logging system, I think any command you prepend 
+    # with deploy will end up in the packages ._deploy_package.log file, useful when testing scripts to see what went wrong
+	deploy deploy-gmenu2x-local
+ }
+
+
+deploy-e2fsprogs(){
+	deploy deploy_binaries
+}
+
+PACKAGES+=" viewimage"
+hset viewimage url "http://dl.dropbox.com/u/12747635/parrot_df3120/src/viewimage.tar.gz"
+hset viewimage depends " libsdl libsdlimage libsdlgfx"
+
+compile-viewimage(){
+	make clean && make
+}
+
+deploy-viewimage(){
+	deploy cp ./viewimage "$ROOTFS"/usr/bin
 }
