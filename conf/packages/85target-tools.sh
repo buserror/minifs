b7226aae248e4534c7e0b9b10f323ae8103d3b1e
Michel Pollet <buserror@gmail.com>
2012-11-19 13:59:50 +0000

    target-tools: Fixed
    fixed ... again
    
    should work now
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/85target-tools.sh
+++ conf/packages/85target-tools.sh
@@ -25,7 +25,7 @@ PACKAGES+=" targettools"
 hset targettools url "none"
 hset targettools dir "."
 hset targettools destdir "$STAGING_USR"
-#hset targettools tools ""
+hset targettools tools "all"
 hset targettools depends "syslinux"
 
 configure-targettools() {
@@ -33,28 +33,22 @@ configure-targettools() {
 }
 compile-targettools() {
 	local tools=$(hget targettools tools)
-	if [ "$tools" != "" ]; then
-		tools="TOOLS=$tools"
-	fi
 	compile-generic \
 		-C $CONF_BASE/target-tools \
 		STAGING="$STAGING_USR" \
 		LDFLAGS="$LDFLAGS_BASE" \
-		"$tools"
+		TOOLS="$tools"
 }
 install-targettools() {
 	log_install echo Done
 }
 deploy-targettools() {
 	local tools=$(hget targettools tools)
-	if [ "$tools" != "" ]; then
-		tools="TOOLS=$tools"
-	fi
 	deploy make \
 		-C $CONF_BASE/target-tools \
 		STAGING="$STAGING_USR" \
 		ROOT="$ROOTFS" \
-		"$tools"
+		TOOLS="$tools" \
 		deploy
 }
 
