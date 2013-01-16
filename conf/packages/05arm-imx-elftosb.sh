2d57109fea30a55f82279616757435cbac5c5abe
Michel Pollet <buserror@gmail.com>
2013-01-16 16:03:53 +0000

    elftosb: Moved to main package dir
    Since mx23 and 28 uses it
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/05arm-imx-elftosb.sh
+++ conf/packages/05arm-imx-elftosb.sh
@@ -1,17 +1,22 @@
-
+#
+# imx23/imx28 bootlet generator
+#
 PACKAGES+=" elftosb"
 hset elftosb url "git!git://github.com/buserror-uk/Olinuxino-Micro-Bootlets.git#Olinuxino-Bootlets.tar.bz2"
 hset elftosb depends "linux-dtb"
 hset elftosb phases "deploy"
+hset elftosb board "stmp378x_dev"
 
 deploy-elftosb-local() {
 	ln -sf $(hget linux-dtb filename) zImage
 	rm -f mv sd_mmc_bootstream.raw
-	echo $TARGET_KERNEL_CMDLINE >linux_prep/cmdlines/stmp378x_dev.txt
-	echo $TARGET_KERNEL_CMDLINE >>linux_prep/cmdlines/stmp378x_dev.txt
+	echo $TARGET_KERNEL_CMDLINE >linux_prep/cmdlines/$(hget elftosb board).txt
+#	echo $TARGET_KERNEL_CMDLINE >>linux_prep/cmdlines/$(hget elftosb board).txt
 	$MAKE \
-		CROSS_COMPILE="${CROSS}-" && \
-		mv sd_mmc_bootstream.raw ..
+		CROSS_COMPILE="${CROSS}-" \
+		BOARD=$(hget elftosb board) && \
+		mv sd_mmc_bootstream.raw .. && \
+		cp elftosb-*/bld/linux/elftosb ../staging-tools/bin/
 }
 
 deploy-elftosb() {
