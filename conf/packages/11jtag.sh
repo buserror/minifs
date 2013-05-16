1f5f0712d17e3b941c169491f64304fe6bc36be1
Michel Pollet <buserror@gmail.com>
2013-05-16 17:57:20 +0100

    urjtag: Added urjtag for programming flash/fpga
    Compile a lightweight utjtag with the gpio driver

---
+++ conf/packages/11jtag.sh
@@ -0,0 +1,19 @@
+
+PACKAGES+=" urjtag"
+hset urjtag url "git!git://urjtag.git.sourceforge.net/gitroot/urjtag/urjtag#urjtag-git.tar.bz2"
+hset urjtag dir "urjtag/urjtag"
+
+patch-urjtag() {
+	local v=$(gettext --version | sed -n -r 's/.*\s([0-9]*\.[0-9]*\.[0-9]*)$/\1/p')
+	sed -i -e "s|^AM_GNU_GETTEXT_VERSION.*$|AM_GNU_GETTEXT_VERSION([$v])|" configure.ac
+}
+configure-urjtag() {
+	configure-generic \
+		--without-readline --without-libusb --without-libftdi \
+		--disable-python --disable-bus --disable-bsdl \
+		--enable-cable=gpio \
+		--enable-lowlevel=direct
+}
+deploy-urjtag() {
+	deploy deploy_binaries
+}
