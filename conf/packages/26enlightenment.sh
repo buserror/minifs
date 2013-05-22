fb430f79bd2b7ce27d856237e66e338917acde3b
Michel Pollet <buserror@gmail.com>
2013-05-22 12:12:18 +0100

    e17: Added libeio and libecore
    Enlightnment libraries

--- conf/packages/26enlightenment.sh
+++ conf/packages/26enlightenment.sh
@@ -10,3 +10,13 @@ PACKAGES+=" libevas"
 hset libevas url "http://download.enlightenment.org/releases/evas-1.7.7.tar.gz"
 hset libevas depends "libpixman libeet"
 hset libevas optional "libjpeg libpng libpixman libharfbuzz"
+
+PACKAGES+=" libeio"
+hset libeio url "http://download.enlightenment.org/releases/eet-1.7.7.tar.gz"
+hset libeio depends "libeet"
+
+PACKAGES+=" libecore"
+hset libecore url "http://download.enlightenment.org/releases/ecore-1.7.7.tar.gz"
+hset libecore depends "libeio libevas"
+
+
