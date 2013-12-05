161a1022c95fb87c8974c4ee9526862934452c86
Michel Pollet <buserror@gmail.com>
2013-12-05 16:48:58 +0000

    pkg-config: Fixed a potential bug
    Need to ensire a slash is present when inserting the staging path

--- conf/host-tools/pkg-config.c
+++ conf/host-tools/pkg-config.c
@@ -1,23 +1,23 @@
 /*
  * pkg-config.c
- * 
+ *
  * (C) 2008-2011 Michel Pollet <buserror@gmail.com>
- * 
- * this program sits under pkg-config and makes sure the 
- * pathnames returned are all properly relative to the staging 
+ *
+ * this program sits under pkg-config and makes sure the
+ * pathnames returned are all properly relative to the staging
  * directory.
  * Since pkg-config is buggy it has to do some massaging
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
@@ -58,7 +58,7 @@ int main(int argc, char * argv[])
 		    !strcmp(argv[i], "--exact-version") ||
 		    !strcmp(argv[i], "--atleast-version"))
 			insert_path = 0;
-	
+
 	nargv[0] = "/usr/bin/pkg-config";
 
 	if (insert_path) {
@@ -73,7 +73,7 @@ int main(int argc, char * argv[])
 	}
 	nargv[di] = NULL;
 	nargc = di;
-	
+
 	fprintf(o, "\n");
 	fflush(stdout);
 
@@ -83,7 +83,7 @@ int main(int argc, char * argv[])
 	pid_t pid = fork();
 	if (pid == 0) {
 	//	setenv("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS", "1", 1);
-	//	setenv("PKG_CONFIG_ALLOW_SYSTEM_LIBS", "1", 1);		
+	//	setenv("PKG_CONFIG_ALLOW_SYSTEM_LIBS", "1", 1);
 		close(1);
 		dup2(pipefd[1], 1);
 		execv(nargv[0], nargv);
@@ -92,7 +92,7 @@ int main(int argc, char * argv[])
 	rd = read(pipefd[0], buf, sizeof(buf)-1);
 	waitpid(pid, &ex, 0);
 	ex =  WEXITSTATUS(ex);
-	
+
 	buf[rd] = 0;
 	while (rd > 0) {
 		if (buf[rd-1] != '\n')
@@ -102,13 +102,13 @@ int main(int argc, char * argv[])
 	if (rd > 0)
 		fprintf(o, "%s %s\n", package, buf);
 	else
-		fprintf(o, "%s (return %d)\n", package, WEXITSTATUS(ex));		
+		fprintf(o, "%s (return %d)\n", package, WEXITSTATUS(ex));
 
 
 	char out[8192];
 	char *src = buf, *dst = out;
 	int space = 1;
-	
+
 	while (*src) {
 		if (space) {
 			int l = 0;
@@ -117,15 +117,15 @@ int main(int argc, char * argv[])
 			*dst = 0;
 			if (src[0] == '/' && strncmp(src, staging, stagingl))
 				sprintf(dst, "%s", staging);
-			else if (src[0] == '-' && 
+			else if (src[0] == '-' &&
 				  (src[1] == 'L' || src[1] == 'I') &&
 				  strncmp(src + 2, staging, stagingl)) {
-				sprintf(dst, "%c%c%s", src[0], src[1], staging);
+				sprintf(dst, "%c%c%s/", src[0], src[1], staging);
 				src += 3;
 			}
 			dst += strlen(dst);
 		}
-		
+
 		space = *src == ' ';
 		*dst++ = *src++;
 	}
@@ -136,6 +136,6 @@ int main(int argc, char * argv[])
 	if (*out) {
 		write(1, out, dst-out); write(1, "\n", 1);
 	}
-	
+
 	exit (ex);
 }
