13c807985235275bb3cfbb34b915caec07261597
Michel Pollet <buserror@gmail.com>
2014-05-29 18:42:04 +0100

    vnc: Updated libvncserver
    To version 0.99

--- conf/packages/25vnc.sh
+++ conf/packages/25vnc.sh
@@ -2,8 +2,9 @@
 
 
 PACKAGES+=" libvncserver"
-hset libvncserver url "http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.7/LibVNCServer-0.9.7.tar.gz"
+#hset libvncserver url "http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.7/LibVNCServer-0.9.7.tar.gz"
 #hset libvncserver url "http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.8.2/LibVNCServer-0.9.8.2.tar.gz"
+hset libvncserver url "http://downloads.sourceforge.net/project/libvncserver/libvncserver/0.9.9/LibVNCServer-0.9.9.tar.gz"
 hset libvncserver depends "zlib libjpeg"
 hset libvncserver configscript "libvncserver-config"
 hset libvncserver optional "libsdl xorglibX11 libgcrypt"
@@ -15,14 +16,14 @@ configure-libvncserver-local() {
 	export LDFLAGS="$LDFLAGS_RLINK"
 
 	# that bit needs SDL somehow. we don't want it if SDL not built
-	if ! env_contains TARGET_PACKAGES libsdl ; then 
-		echo libvncserver Disabling SDL 
+	if ! env_contains TARGET_PACKAGES libsdl ; then
+		echo libvncserver Disabling SDL
 		sed -i -e 's|client_examples||g' \
 			Makefile.am
 		sed -i -e "/client_examples\/Makefile/d" \
 			configure.ac
 	fi
-	if env_contains TARGET_PACKAGES xorglibX11 ; then 
+	if env_contains TARGET_PACKAGES xorglibX11 ; then
 		if env_contains TARGET_PACKAGES libxcb; then
 			LDFLAGS+=" -lxcb"
 		fi
@@ -30,13 +31,13 @@ configure-libvncserver-local() {
 	if ! env_contains TARGET_PACKAGES libgcrypt; then
 		extras+=" --without-gcrypt"
 	fi
-	echo libvncserver Disabling x11vnc 
+	echo libvncserver Disabling x11vnc
 	extras+=" --without-x11vnc"
 	sed -i -e 's|x11vnc||g' \
 			Makefile.am
 	sed -i -e "s|[ \t]\(AC_CONFIG_FILES(\[x11vnc/Makefile\)|true #\1|g" \
 			configure.ac
-	
+
 	libtoolize && $ACLOCAL && autoheader && automake --force-missing --foreign -a -c && autoconf
 	configure-generic-local $extras
 	# 0.9.8.2 uses some crap windows file because of /* #undef LIBVNCSERVER_HAVE_GETTIMEOFDAY */
@@ -74,7 +75,7 @@ hset x11vnc depends "libvncserver"
 configure-x11vnc() {
 	export LDFLAGS="$LDFLAGS_RLINK -lxcb"
 	configure-generic
-	export LDFLAGS="$LDFLAGS_BASE"	
+	export LDFLAGS="$LDFLAGS_BASE"
 }
 
 deploy-x11vnc() {
