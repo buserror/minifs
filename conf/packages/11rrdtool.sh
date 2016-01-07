996e6a9a9602ffab1d91971f2a4cbcb231b862f6
Michel Pollet <buserror@gmail.com>
2016-01-07 14:00:11 +0000

    rrdtool: Updated
    1.5.4 builds with more recent libc, and toolchain
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/11rrdtool.sh
+++ conf/packages/11rrdtool.sh
@@ -1,11 +1,11 @@
 
 PACKAGES+=" rrdtool"
-hset rrdtool url "http://oss.oetiker.ch/rrdtool/pub/rrdtool-1.4.7.tar.gz"
+hset rrdtool url "http://oss.oetiker.ch/rrdtool/pub/rrdtool-1.5.4.tar.gz"
 hset rrdtool depends "libpango libxml2"
 
 configure-rrdtool() {
 	export LDFLAGS="$LDFLAGS_RLINK"
-	configure-generic --disable-nls --disable-perl --disable-lua rd_cv_ieee_works=yes
+	configure-generic --disable-nls --disable-perl --disable-lua --disable-ruby rd_cv_ieee_works=yes
 	export LDFLAGS="$LDFLAGS_BASE"
 }
 
