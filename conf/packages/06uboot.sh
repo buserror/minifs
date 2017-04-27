93a58e10c16fed863e6feab9fe6052c8648ac4cb
Michel Pollet <buserror@gmail.com>
2017-04-27 12:24:53 +0100

    uboot: Now build out of tree
    Use a separate directory
    
    Signed-off-by: Michel Pollet <buserror@gmail.com>

--- conf/packages/06uboot.sh
+++ conf/packages/06uboot.sh
@@ -3,17 +3,20 @@ PACKAGES+=" uboot"
 # uboot URL is set by each board, not here
 #hset uboot url "git!git://repo.or.cz/u-boot-openmoko/parrot-frames.git#uboot-df3120-git.tar.bz2"
 hset uboot target "u-boot.bin"
+hset uboot output "$STAGING/uboot-obj"
 
 configure-uboot() {
 	local arch=$(hget uboot board)
+	mkdir -p $(hget uboot output)
 	if [ "$arch" = "" ];then
 		arch=$MINIFS_BOARD
 		echo WARNING uboot needs an explicit board name
 	fi
-	configure make "$arch"_config
+	configure make O=$(hget uboot output) "$arch"_config
 }
 compile-uboot-local() {
 	$MAKE $MAKE_ARGUMENTS \
+			O=$(hget uboot output) \
 			CROSS_COMPILE="$CROSS-" \
 			$(hget uboot target)
 }
@@ -21,9 +24,8 @@ compile-uboot() {
 	compile compile-uboot-local
 }
 install-uboot-local() {
+	pushd $(hget uboot output)
 	if [ -x tools/mkimage ]; then
-        # Reggie added, need to add "$ROOTFS"/sbin to the made dirs otherwise
-        #fw_printenv fails to get copied across to the rootfs!!
 		mkdir -p "$STAGING_TOOLS"/bin/ "$ROOTFS"/sbin/
 		cp tools/mkimage "$STAGING_TOOLS"/bin/
 	fi
@@ -31,10 +33,9 @@ install-uboot-local() {
 		mkdir -p "$STAGING_USR"/bin/ "$STAGING"/etc/
 		cp tools/env/fw_printenv "$STAGING_USR"/bin/
 		cp tools/env/fw_env.config "$STAGING"/etc/
-        #Reggie added, not sure why but have to shoehorn fw_printenv
-        #onto the rootfs from here, it fails to deploy in the deploy stage!
-        cp tools/env/fw_printenv "$ROOTFS"/sbin/
+		cp tools/env/fw_printenv "$ROOTFS"/sbin/
 	fi
+	popd
 }
 install-uboot() {
 	log_install install-uboot-local
@@ -43,13 +44,14 @@ install-uboot() {
 deploy-uboot-local() {
 	if [ "$(hget uboot deploy-target)" != "" ]; then
 		$MAKE $MAKE_ARGUMENTS \
+				O=$(hget uboot output) \
 				CROSS_COMPILE="$CROSS-" \
 				$(hget uboot deploy-target) &&
 			cp $(hget uboot deploy-target) ..
 	fi
 	if [ -x "$STAGING"/bin/fw_printenv ]; then
 		deploy cp "$STAGING"/usr/bin/fw_printenv "$ROOTFS"/bin/
-	fi	
+	fi
 }
 
 deploy-uboot() {
